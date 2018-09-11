9e9f06c3b83015699a1ef1cce4f14c39a252c2a8
aSkryabina <aSkryabina@users.noreply.github.com>
2018-09-11 17:33:14 +0300

    Добавлен тест удаления контакта

--- fixture/contact.py
+++ fixture/contact.py
@@ -77,3 +77,9 @@ class ContactHelper:
         wd.find_element_by_name("notes").clear()
         wd.find_element_by_name("notes").send_keys(contact.notes)
         wd.find_element_by_name("submit").click()
+
+    def delete_first_contact(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_css_selector("[value=Delete]").click()
+        wd.switch_to_alert().accept()
