397d1a8b0566bc739f013c097a9b86ae37f41299
aSkryabina <aSkryabina@users.noreply.github.com>
2018-09-11 14:12:48 +0300

    Выделен класс для управлениями сессиями

--- fixture/application.py
+++ fixture/application.py
@@ -1,34 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
+from fixture.session import SessionHelper
 
 class Application:
 
     def __init__(self):
         self.wd = webdriver.Chrome()
         self.wd.implicitly_wait(2)
+        self.session = SessionHelper(self)
 
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
 
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_css_selector("[type=submit]").click()
-
     def open_add_new_page(self):
         wd = self.wd
         wd.find_element_by_link_text("add new").click()
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
 
     def open_groups_page(self):
         wd = self.wd
