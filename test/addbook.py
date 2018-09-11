a7a5bf1247783f87d4041269c581693196d44c6a
aSkryabina <aSkryabina@users.noreply.github.com>
2018-09-11 14:25:05 +0300

    Выделены классы для работы с группами и контактами

--- test/addbook.py
+++ test/addbook.py
@@ -12,14 +12,14 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="12345", header="12345", footer="12345"))
+    app.group.create(Group(name="12345", header="12345", footer="12345"))
     app.session.logout()
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_contact(Contact(firstname="Ann", middlename="V.", lastname="Skryabina", nickname="askryabina",
-                                   title="???", company="OOO Big Data Technology", address="St. Petersburg, Novoroshinskaya st. 4",
-                                   home="8152", mobile="931", work="812", fax="", email="a@a.ru", email2="a2@a.ru", email3="a3@a.ru",
-                                   homepage="localhost", bday="17", bmonth="December", byear="1987",
-                                   aday="3", amonth="August", ayear="2017", address2="Murm", phone2="906", notes="All"))
+    app.contact.create(Contact(firstname="Ann", middlename="V.", lastname="Skryabina", nickname="askryabina",
+                               title="???", company="OOO Big Data Technology", address="St. Petersburg, Novoroshinskaya st. 4",
+                               home="8152", mobile="931", work="812", fax="", email="a@a.ru", email2="a2@a.ru", email3="a3@a.ru",
+                               homepage="localhost", bday="17", bmonth="December", byear="1987",
+                               aday="3", amonth="August", ayear="2017", address2="Murm", phone2="906", notes="All"))
     app.session.logout()
